6643432f3505411227481a30b4d1216a48b9399a
Kayalvizhi Thirumavalavan <tkayalvizhi@outlook.com>
2021-07-06 11:32:21 +0530

    "with relevant comments and some error handling"

--- my-app/src/main/java/com/mycompany/app/App.java
+++ my-app/src/main/java/com/mycompany/app/App.java
@@ -16,15 +16,15 @@ import com.amazonaws.services.simplesystemsmanagement.model.GetParameterResult;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
+import java.security.InvalidParameterException;
 import java.util.HashMap;
 import java.util.Map;
 
-/**
- * Hello world!
- *
- */
+
 public class App implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent>
 {
+
+     /* defined outside handler for Execution context reuse */
     private static final AWSSimpleSystemsManagement ssm = AWSSimpleSystemsManagementClientBuilder.defaultClient();
     private static final AmazonDynamoDB dynamoDB = AmazonDynamoDBClientBuilder.defaultClient();
 
@@ -37,7 +37,9 @@ public class App implements RequestHandler<APIGatewayProxyRequestEvent, APIGatew
 
     protected GetItemResult readWardData(APIGatewayProxyRequestEvent event){
         Map<String, String> queryParams = event.getQueryStringParameters();
+        /* get tableName from parameter store */
         String tableName = getTableName();
+        /* get key which contains {"id": id_number from query} */
         Map<String, AttributeValue> key = getKey(queryParams);
         GetItemRequest getItemRequest = new GetItemRequest().withTableName(tableName).withKey(key);
 
@@ -47,7 +49,7 @@ public class App implements RequestHandler<APIGatewayProxyRequestEvent, APIGatew
     }
 
     private static String getTableName(){
-        GetParameterRequest tableParameterRequest = new GetParameterRequest().withName("Keepr-ward-data").withWithDecryption(false);
+        GetParameterRequest tableParameterRequest = new GetParameterRequest().withName("ward-data").withWithDecryption(false);
         GetParameterResult tableResult = ssm.getParameter(tableParameterRequest);
         return tableResult.getParameter().getValue();
     }
@@ -56,18 +58,21 @@ public class App implements RequestHandler<APIGatewayProxyRequestEvent, APIGatew
         if(queryParams.containsKey("ward_id")) {
 
             Map<String, AttributeValue> key = new HashMap<>();
+            /* withN() converts AttributeValue to number */
             key.put("id", new AttributeValue().withN(queryParams.get("ward_id")));
 
             return key;
 
         }
-        return null;
+
+        else throw new IllegalArgumentException("param : ward_id is missing");
     }
 
     private static APIGatewayProxyResponseEvent generateResponse(GetItemResult wardData) {
         Gson gson = new GsonBuilder().setPrettyPrinting().create();
         APIGatewayProxyResponseEvent response = new APIGatewayProxyResponseEvent();
         response.setStatusCode(200);
+        /* convert wardData object to json*/
         response.setBody(gson.toJson(wardData));
 
         return response;
